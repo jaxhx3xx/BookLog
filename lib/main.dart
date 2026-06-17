b30e175ae682ea53c9ac1cfb492b27ad992d09ee
jaxhx3xx <jaehuij660@gmail.com>
2026-06-17 15:21:57 +0900

    테스트용 데이터 삽입

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,27 @@
 import 'package:flutter/material.dart';
 import 'di/locator.dart';
 import 'screens/home_screen.dart';
+import 'package:drift/drift.dart';
+import 'database/app_database.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
   setupLocator();
+
+  final db = getIt<LocalDatabase>();
+
+  await db.createBook(
+    BooksCompanion.insert(
+      title: '해리포터',
+      author: 'J.K. 롤링',
+      readDate: DateTime.now(),
+      rating: 5,
+      memo: '재밌었다',
+      status: '읽음',
+    ),
+  );
+
   runApp(const MyApp());
 }
 
